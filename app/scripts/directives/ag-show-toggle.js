ffb25ca7f1c364ea48830b10366b1ae20321d016
Yujun Wu <wyj0912@gmail.com>
2014-12-04 20:29:57 -0800

    Re-layout help page

---
+++ app/scripts/directives/ag-show-toggle.js
@@ -0,0 +1,40 @@
+(function(angular, $) {
+  'use strict';
+
+  var dir = function() {
+    return {
+      scope: {
+        target: '@' // id or class or tag
+      },
+      link: function(scope, elem) {
+        var TOGGLE_CLASS = 'ag-show-toggle',
+            HIDE_CLASS = 'ag-hide';
+
+        var state = 0, // 0: hide, 1: show
+            $target;
+
+        if (!scope.target) {
+          return;
+        }
+
+        if (scope.target[0] === '#') { // id
+          $target = $(scope.target);
+        } else { // class or tag: search in siblings
+          $target = $(elem[0]).parent().find(scope.target);
+        }
+
+        if ($target) {
+          $target.addClass(TOGGLE_CLASS);
+          $target.addClass(HIDE_CLASS);
+          elem.on('click', function() {
+            $target.toggleClass(HIDE_CLASS);
+            state = state === 1 ? 0 : 1;
+          });
+        }
+      }
+    };
+  };
+
+  angular.module('AgoraApp').directive('agShowToggle', dir);
+
+})(window.angular, window.Zepto);
--- app/scripts/directives/ag-subscribe-form.js
+++ app/scripts/directives/ag-subscribe-form.js
@@ -1,20 +1,49 @@
-(function(angular) {
+(function(angular, ga) {
   'use strict';
 
+  function sendUserInGA(user) {
+    var CATEGORY = 'subscribe',
+        ACTION = 'submit',
+        label = JSON.stringify(user);
 
+    ga('send', 'event', CATEGORY, ACTION, label);
+  }
 
-  var dir = function($templaceCache) {
+  var dir = function($templateCache) {
     return {
       restrict: 'A',
-      template: $templateCache.get('directives/subscribe_form'),
+      template: $templateCache.get('directives/subscribe_form.html'),
       replace: true,
-      controller: function($scope) {
-        $scope.submit = function(user) {
+      scope: {},
+      controller: function($scope, $interval) {
+        function onSuccess() {
+          var DURATION = 3000;
+          $scope.user = {};
 
+          $scope.success = true;
+
+          $interval(function() {
+            $scope.success = false;
+          }, DURATION);
+        }
+
+        $scope.success = false;
+        $scope.user = {};
+
+        $scope.submit = function() {
+          var user = $scope.user;
+
+          if(user.name && user.email) {
+            window.console.log(user);
+            sendUserInGA(user);
+            onSuccess();
+          } else {
+            window.alert('Please enter full name and email');
+          }
         };
       }
     };
   };
 
   angular.module('AgoraApp').directive('agSubscribeForm', dir);
-})(window.angular);
+})(window.angular, window.ga);
