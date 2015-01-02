34e698509a73b2f2b56dcdb2d8877794c7fb5cd1
Yujun Wu <wyj0912@gmail.com>
2015-01-02 01:05:30 -0800

    Local create db and init users table

---
+++ migrations/20150102083302-add-users.js
@@ -0,0 +1,17 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  db.createTable('users', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    email: { type: 'string', unique: true, notNull: true },
+    password: { type: 'string', unique: true, notNulll: true },
+    name: { type: 'string' },
+    company_name: { type: 'string' },
+    description: { type: 'text'}
+  }, callback);
+};
+
+exports.down = function(db, callback) {
+  db.dropTable('users');
+};
