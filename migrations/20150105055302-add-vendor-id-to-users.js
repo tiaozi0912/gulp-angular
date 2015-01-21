c8913a283901230c65d73ac9cffdf3a80560257b
Yujun Wu <wyj0912@gmail.com>
2015-01-21 05:16:15 +0000

    Migrations

--- migrations/20150105055302-add-vendor-id-to-users.js
+++ migrations/20150105055302-add-vendor-id-to-users.js
@@ -2,11 +2,11 @@ var dbm = require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-  db.addColumn('users', 'vendor_id', { type: 'int' }, function(err) {
-    if (err) { callback(err); return; }
+  //db.addColumn('users', 'vendor_id', { type: 'int' }, function(err) {
+   // if (err) { callback(err); return; }
 
-    db.addIndex('users', 'index_users_on_vendor_id', ['vender_id'], true, callback);
-  });
+    db.addIndex('users', 'index_users_on_vendor_id', ['vendor_id'], true, callback);
+  //});
 };
 
 exports.down = function(db, callback) {
