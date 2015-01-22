3890e740e7b6a789f42f2048d1372325cb63db37
Yujun Wu <wyj0912@gmail.com>
2015-01-21 17:51:07 -0800

    Remove imagemin in the images task cuz it causes error in production

--- gulpfile.js
+++ gulpfile.js
@@ -74,11 +74,11 @@ gulp.task('html', ['sprite', 'views', 'styles', 'templates'], function () {
 
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-    .pipe($.filter('**/*.{jpg,jpeg,svg,gif,png}'))
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
+    // .pipe($.filter('**/*.{jpg,jpeg,svg,gif,png}'))
+    // .pipe($.cache($.imagemin({
+    //   progressive: true,
+    //   interlaced: true
+    // })))
     .pipe(gulp.dest('dist/images'));
 });
 
